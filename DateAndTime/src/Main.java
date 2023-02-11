206ba544a6e72d0cef2591cdd08a66db1fe8aaac
Dmitriy <120496900+Dmitriynep@users.noreply.github.com>
2023-02-11 22:39:56 +0300

    Update Main.java

--- DateAndTime/src/Main.java
+++ DateAndTime/src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Дата и время: ");
         System.out.println(LocalDateTime.now());
+        
+        //another Cricosiabra not the same as local repo ! )))
     }
 }
